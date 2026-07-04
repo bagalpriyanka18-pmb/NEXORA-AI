b165aa9ff374c8a352f012f7b38702de5912535d
Priyanka Bagal <your-email@gmail.com>
2026-07-04 12:38:28 +0530

    fix static css path

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+
 from fastapi.templating import Jinja2Templates
 
 import yfinance as yf
